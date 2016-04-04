a3adb20fec5253ee93b359d1faf7de6d0e0c1cfb
henry <hlyford11@gmail.com>
2016-04-04 10:57:52 -0700

    code clean up

--- client/app/slots/slots.js
+++ client/app/slots/slots.js
@@ -7,17 +7,14 @@ angular.module('slotmachine.slots', [])
 		{result: 2, imgSlug: "../images/tea2.png"},
 		{result: 3, imgSlug: "../images/espresso3.png"},
 	]
-	$scope.spinning = false, $scope.reward;
+	$scope.reward;
 
 
 	var drinkOptions = ["coffee", "tea", "espresso"];
 	
-	$scope.leverClick = function() {
-		// switch state to spinning
-		$scope.spinning = true;
+	$scope.leverClick = function() {	
 		// spin and select results
 		selectResult();
-
 	}
 
 	// conducts an individual random selection of the reels
@@ -29,22 +26,20 @@ angular.module('slotmachine.slots', [])
 			randomNumber = Math.floor(Math.random() * 3);			
 			spinResults[i] = randomNumber;
 		}				
-		// change state to reflect the results		
+		// change state to reflect the results				
 		$scope.slotData[0].result = spinResults[0], $scope.slotData[0].imgSlug = "../images/" + drinkOptions[spinResults[0]] + "1.png";
 		$scope.slotData[1].result = spinResults[1], $scope.slotData[1].imgSlug = "../images/" + drinkOptions[spinResults[1]] + "2.png";
-		$scope.slotData[2].result = spinResults[2], $scope.slotData[2].imgSlug = "../images/" + drinkOptions[spinResults[2]] + "3.png";		
-		
-		console.log('state', spinCount, ' for 1 ', $scope.slotData[0]);
+		$scope.slotData[2].result = spinResults[2], $scope.slotData[2].imgSlug = "../images/" + drinkOptions[spinResults[2]] + "3.png";				
+				
 		// call spin again or go into the callback to check if winner
-		if (spinCount < 1) {
-			console.log('at ', spinCount, ' : ', $scope.slotData);
+		if (spinCount < -12 ) {			
 			setTimeout(function() {
-				return spin(spinCount + 1, callback);			
-			}, 200);			
-		} else {			
-			console.log($scope.slotData[0].imgSlug);
+				spin(spinCount + 1, callback);			
+			}, 1000);						
+		} else {						
 			callback();	
 		}
+		return;
 	}
 	// starts the spinning then determines if user has won
 	function selectResult () {					
