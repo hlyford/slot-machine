428cd848d3bcfe90751e453b03fe1327b7f80425
henry <hlyford11@gmail.com>
2016-04-04 08:33:09 -0700

    Adds effects and cleans up UI; note: placeholder in place for checking if user won

--- client/app/slots/slots.js
+++ client/app/slots/slots.js
@@ -15,15 +15,15 @@ angular.module('slotmachine.slots', [])
 	$scope.leverClick = function() {
 		// switch state to spinning
 		$scope.spinning = true;
-		// start spinner
-
-		// select results
+		// spin and select results
 		selectResult();
 
 	}
 
-	function selectResult () {		
+	// conducts an individual random selection of the reels
+	function spin (spinCount, callback) {		
 		var spinResults = [], randomNumber, imgSlug;
+		
 		for (var i = 0; i < 3; i++) {
 			// select a number between 1 and 3 (inclusive)
 			randomNumber = Math.floor(Math.random() * 3);			
@@ -33,17 +33,44 @@ angular.module('slotmachine.slots', [])
 		$scope.slotData[0].result = spinResults[0], $scope.slotData[0].imgSlug = drinkOptions[spinResults[0]] + "1";
 		$scope.slotData[1].result = spinResults[1], $scope.slotData[1].imgSlug = drinkOptions[spinResults[1]] + "2";
 		$scope.slotData[2].result = spinResults[2], $scope.slotData[2].imgSlug = drinkOptions[spinResults[2]] + "3";
-		// see if user won
-		if (spinResults[0] === spinResults[1] && spinResults[0] === spinResults[2])	{
-			console.log("Winner!");
-			// determine the reward
-			if (spinResults[0] === 0) {$scope.reward = "coffee"; }
-			else if (spinResults[0] === 1) {$scope.reward = "tea"; }
-			else {$scope.reward = "espresso"; }
-
-			$(".bs-example-modal-sm").modal();
-
+		
+		// call spin again or go into the callback to check if winner
+		if (spinCount < 5) {
+			console.log('at ', spinCount, ' : ', $scope.slotData);
+			setTimeout(function() {
+				return spin(spinCount + 1, callback);			
+			}, 100);			
+		} else {			
+			callback();	
 		}
+	}
+	// starts the spinning then determines if user has won
+	function selectResult () {					
+		// remove border if user has just won
+		$('.reel-container').removeClass('orange-border');
+
+		// callback checks if user has won
+		spin(0, function() {			
+			console.log($scope.slotData);
+			if (true) {
+			// if ($scope.slotData[0].result === $scope.slotData[1].result && $scope.slotData[0].result === $scope.slotData[2].result)	{
+				console.log("Winner!");
+				// determine the reward
+				if ($scope.slotData[0].result === 0) {$scope.reward = "coffee"; }
+				else if ($scope.slotData[0].result === 1) {$scope.reward = "tea"; }
+				else {$scope.reward = "espresso"; }
+				// flash the border
+				var flashInterval = setInterval(function() {
+    			$('.reel-container').toggleClass('orange-border');
+				}, 100);		
+				// stop the border flash but keep border, show prize		
+				setTimeout(function() {					
+					window.clearInterval(flashInterval);
+					$('.reel-container').addClass('orange-border');
+					$(".bs-example-modal-sm").modal();
+				}, 1000);
+			}
+		});		
 	}
 
 
--- notes.md
+++ notes.md
@@ -1,6 +1,13 @@
 - put images in front of grid that has events bound
 - click the button, set timeout, thing spins, show results
 
-
-- thumbtack logo and red button on machine
 - tests 
+
+1) scramble icons as state changes; end on final state; call spin recursively with intervals getting longer until stopping on one
+- what if it hits the same one??
+
+2) CSS animaition to roll through the list of items
+
+
+other
+- flash orange around border when win
